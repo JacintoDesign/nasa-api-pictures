d9711f30d64bc1b4d5092fa534df9377f36e7b6e
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-23 09:40:46 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const favoritesContainer = document.getElementById('favorites');
+const resultsContainer = document.getElementById('results');
+
+function getNasaPictures() {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=hHt592uvYa5AKdj9zIGFy4UoOLvHCVQsJh2xmRdL&count=10')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        let resultsArray = data; 
+
+        resultsContainer.innerHTML = resultsArray.map(
+          result => `
+          <div class="card">
+          <a href="${result.hdurl}" title="View Full Image" target="blank" rel="noopener noreferrer">
+            <img class="card-img-top" src="${result.url}" alt="NASA Picture of the Day">
+          </a>      
+          <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <!--<p class="save-text" onclick="saveFavorite(result)">Add To Favorites</p>-->
+            <p class="card-text">${result.explanation}</p>
+            <p class="card-text"><small class="text-muted">
+              <strong>${result.date}</strong>
+              <span>${result.copyright}</span>
+            </small></p>
+          </div>
+        </div>
+          `  
+        ).join('');
+
+    });
+}
+
+getNasaPictures();
