73dd0978a1feb3836c0a944d5b6498ceeb3de7cb
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-23 17:46:56 -0500

    adding favorites functionality, load more, favorites selection

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const favoritesContainer = document.getElementById('favorites');
 const resultsContainer = document.getElementById('results');
 
 let resultsArray = [];
+let favoritesArray = [];
 
 function getNasaPictures() {
     fetch('https://api.nasa.gov/planetary/apod?api_key=hHt592uvYa5AKdj9zIGFy4UoOLvHCVQsJh2xmRdL&count=10')
@@ -9,12 +10,12 @@ function getNasaPictures() {
     .then(data => {
         // console.log(data);
         resultsArray = data; 
-        updateDOM();
+        updateResultsDOM();
     });
 }
 
 function getMorePictures() {
-    fetch('https://api.nasa.gov/planetary/apod?api_key=hHt592uvYa5AKdj9zIGFy4UoOLvHCVQsJh2xmRdL&count=10')
+    fetch('https://api.nasa.gov/planetary/apod?api_key=hHt592uvYa5AKdj9zIGFy4UoOLvHCVQsJh2xmRdL&count=3')
     .then(res => res.json())
     .then(data => {
         // console.log(data);
@@ -23,37 +24,88 @@ function getMorePictures() {
             // console.log(additionalResult);
             resultsArray.push(additionalResult);
         }
-        updateDOM();
+        updateResultsDOM();
     });
+
 }
 
-function updateDOM() {
+function updateResultsDOM() {
     console.log(resultsArray);
-    for (let i = 0; i < resultsArray.length; i++) {
+    let i;
+    for (i = 0; i < resultsArray.length; i++) {
         resultsArray[i].copyright
         if (resultsArray[i].copyright == undefined) {
             resultsArray[i].copyright = "";
             //console.log(resultsArray[i].copyright);
         }
     }
-    resultsContainer.innerHTML = resultsArray.map(
+    resultsContainer.innerHTML += resultsArray.map(
         result => `
         <div class="card">
-        <a href="${result.hdurl}" title="View Full Image" target="blank" rel="noopener noreferrer">
-          <img class="card-img-top" src="${result.url}" alt="NASA Picture of the Day">
-        </a>      
-        <div class="card-body">
-          <h5 class="card-title">${result.title}</h5>
-          <!--<p class="save-text" onclick="saveFavorite(result)">Add To Favorites</p>-->
-          <p class="card-text">${result.explanation}</p>
-          <p class="card-text"><small class="text-muted">
-            <strong>${result.date}</strong>
-            <span>${result.copyright}</span>
-          </small></p>
+            <a href="${result.hdurl}" title="View Full Image" target="blank" rel="noopener noreferrer">
+            <img class="card-img-top" src="${result.url}" alt="NASA Picture of the Day">
+            </a>      
+            <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <p class="save-text" onclick="saveFavorite('${result.url}')">Add To Favorites</p>
+            <p class="card-text">${result.explanation}</p>
+            <p class="card-text"><small class="text-muted">
+                <strong>${result.date}</strong>
+                <span>${result.copyright}</span>
+            </small></p>
+            </div>
         </div>
-      </div>
-      `  
+        `  
       ).join('');
+}
+
+function updateFavoritesDOM() {
+    console.log(favoritesArray);
+    let i;
+    for (i = 0; i < favoritesArray.length; i++) {
+        favoritesArray[i].copyright
+        if (favoritesArray[i].copyright == undefined) {
+            favoritesArray[i].copyright = "";
+            //console.log(favoritesArray[i].copyright);
+        }
+    }
+    favoritesContainer.innerHTML = favoritesArray.map(
+        result => `
+        <div class="card">
+            <a href="${result.hdurl}" title="View Full Image" target="blank" rel="noopener noreferrer">
+            <img class="card-img-top" src="${result.url}" alt="NASA Picture of the Day">
+            </a>      
+            <div class="card-body">
+            <h5 class="card-title">${result.title}</h5>
+            <p class="save-text" onclick="saveFavorite('${result.url}')">Add To Favorites</p>
+            <p class="card-text">${result.explanation}</p>
+            <p class="card-text"><small class="text-muted">
+                <strong>${result.date}</strong>
+                <span>${result.copyright}</span>
+            </small></p>
+            </div>
+        </div>
+        `  
+      ).join('');
+}
+
+function saveFavorite(result) {
+    for (i = 0; i < resultsArray.length; i++) {
+        if (resultsArray[i].url.includes(result)) {
+            let found = resultsArray[i];
+            console.log(found);
+            favoritesArray.push(found);
+            console.log(favoritesArray);
+        }
+    }
+}
+
+function showFavorites() {
+    favoritesContainer.classList.add('display-block');
+    favoritesContainer.classList.remove('display-none');
+    resultsContainer.classList.add('display-none');
+    resultsContainer.classList.remove('display-block');
+    updateFavoritesDOM();
 }
 
 getNasaPictures();
